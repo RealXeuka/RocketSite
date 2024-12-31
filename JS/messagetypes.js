12bc9cfe63f898fb8d22a7f6864fe81eda93a250
RealXeuka <139843297+RealXeuka@users.noreply.github.com>
2024-12-31 02:41:07 +0100

    Add files via upload

---
+++ JS/messagetypes.js
@@ -0,0 +1,524 @@
+document.querySelectorAll('.box1').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box2').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box3').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box4').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box5').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box6').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box7').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box8').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box9').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box10').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box11').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box12').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box13').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box15').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
+
+document.querySelectorAll('.box').forEach(box => {
+  let timeout; 
+  box.addEventListener('mousedown', () => {
+    timeout = setTimeout(() => {
+      const codeText = box.querySelector('pre').innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+);
+box.addEventListener('mouseup', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('mouseleave', () => {
+  clearTimeout(timeout);
+  }
+);
+box.addEventListener('touchstart', () => {
+  timeout = setTimeout(() => {
+    const codeText = box.querySelector('pre').innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+        }
+      )
+    }, 500)
+  }
+)
+box.addEventListener('touchend', () => {
+  clearTimeout(timeout);
+      }
+    )
+  }
+)
