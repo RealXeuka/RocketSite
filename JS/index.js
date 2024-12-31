3eb31a75e27fd2a6856ca60f997f0bbcbe4394fa
RealXeuka <139843297+RealXeuka@users.noreply.github.com>
2024-12-31 04:05:00 +0100

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,19 @@
+async function getDetails() {
+  try {
+    const response = await fetch('https://ipapi.co/json/');
+    const data = await response.json();
+    const userAgent = navigator.userAgent;
+
+    const deviceType = /Mobi|Android|iPhone/.test(userAgent) ? "Mobile" : "Desktop";
+
+    document.getElementById('ipDisplay').innerHTML = `IP Address: ${data.ip} <br>City: ${data.city} <br>ZIP Code: ${data.postal} <br>Country: ${data.country_name} <br>Device: ${deviceType}`;
+  } catch (error) {
+    document.getElementById('ipDisplay').textContent = 'Unable to fetch details.';
+  }
+}
+
+getDetails();
+
 function toggleNavbar() {
   const navbar = document.getElementById("navbar");
   if (navbar.style.transform === "translateY(0%)") {
